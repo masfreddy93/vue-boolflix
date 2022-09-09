de6051bbf1b668cd50c2a955fa5cada07ce490f5
masfreddy93 <masieronicola@gmail.com>
2022-09-09 15:12:32 +0200

    create MainComponent

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,19 @@
 <template>
   <div id="app">
     <HeaderComponent msg="Boolflix"/>
+    <MainComponent />
   </div>
 </template>
 
 <script>
 import HeaderComponent from './components/HeaderComponent.vue';
+import MainComponent from './components/MainComponent.vue';
 
 export default {
   name: 'App',
   components: {
-    HeaderComponent
+    HeaderComponent,
+    MainComponent
 }
 }
 </script>
---
+++ src/components/MainComponent.vue
@@ -0,0 +1,16 @@
+<template>
+    <main>
+        <ul>
+            <li>1st movie</li>
+            <li>2nd movie</li>
+        </ul>
+    </main>
+</template>
+
+
+<script>
+
+    export default {
+    }
+
+</script>
