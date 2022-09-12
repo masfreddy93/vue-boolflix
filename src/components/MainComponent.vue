3dd81dce4536dbd1da93d78a3f6b7598e943d091
masfreddy93 <masieronicola@gmail.com>
2022-09-12 12:04:53 +0200

    change attribute's name to computed movieFound

--- src/components/MainComponent.vue
+++ src/components/MainComponent.vue
@@ -1,14 +1,11 @@
 <template>
     <main>
-        <!-- <ul v-for="(movie,i) of moviesFounded" :key="i">
-            <li> {{ movie }} </li>
-        </ul> -->
         <ul v-for="(movie, i) of moviesFound" :key="i">
             <li>
                 <h4>Title: {{movie.title}} </h4>
-                <h4>Original Title: {{movie.original_title}}</h4>
-                <h4>Language: {{movie.original_language}}</h4>
-                <h4>Vote: {{movie.vote_average}}</h4>
+                <h4>Original Title: {{movie.originalTitle}}</h4>
+                <h4>Language: {{movie.lang}}</h4>
+                <h4>Vote (0/5): {{movie.vote}}</h4>
                 <br>
             </li>
         </ul>
@@ -21,21 +18,23 @@
     import state from '../store.js'
 
     export default {
-
         computed: {
             moviesFound() {
-                return state.moviesFound;
+                // return state.moviesFound;
+                return  state.moviesFound.map((el) => {
+                    const newMovie = {
+                        id: el.id,
+                        title: el.title,
+                        originalTitle: el.original_title,
+                        lang: el.original_language,
+                        flag: '',
+                        poster: '',
+                        vote: Math.round(el.vote_average / 2),
+                    }
+                    return newMovie;
+                })
             },
-            // arrayProva() {
-            //     return state.arrayProva;
-            // }
         },
-
-        // data() {
-        //     return {
-                
-        //     }
-        // }
     }
 
 </script>
