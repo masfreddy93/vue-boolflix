75a213e6d3698aa4efb4caf3dabf11e6a0ddbf8e
masfreddy93 <masieronicola@gmail.com>
2022-09-12 12:55:17 +0200

    add flag description

--- src/components/MainComponent.vue
+++ src/components/MainComponent.vue
@@ -4,7 +4,11 @@
             <li>
                 <h4>Title: {{movie.title}} </h4>
                 <h4>Original Title: {{movie.originalTitle}}</h4>
-                <h4>Language: {{movie.lang}}</h4>
+                <!-- <h4>Language: {{movie.lang}}</h4> -->
+                <h4>Language: 
+                    <img  :src="movie.flag" alt="flag of country's language" width="20"> 
+                </h4> 
+                <!-- <img :src="movie.flag" alt="" width=""> -->
                 <h4>Vote (0/5): {{movie.vote}}</h4>
                 <br>
             </li>
@@ -18,6 +22,13 @@
     import state from '../store.js'
 
     export default {
+
+        data() {
+            return {
+                flagBaseUri: 'https://flagcdn.com'      
+            }
+        },
+
         computed: {
             moviesFound() {
                 // return state.moviesFound;
@@ -27,7 +38,8 @@
                         title: el.title,
                         originalTitle: el.original_title,
                         lang: el.original_language,
-                        flag: '',
+                        flag: `${this.flagBaseUri}/${el.original_language}.svg`,
+                        // flag: 'https://flagcdn.com/za.svg',
                         poster: '',
                         vote: Math.round(el.vote_average / 2),
                     }
@@ -35,6 +47,7 @@
                 })
             },
         },
+
     }
 
 </script>
